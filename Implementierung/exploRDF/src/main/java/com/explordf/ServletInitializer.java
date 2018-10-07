40b80e1b4ba25811ce53b24ffd556f8e02396272
AndNied <dienand88@yahoo.com>
2018-10-07 14:10:17 +0200

    Initial commit

---
+++ Implementierung/exploRDF/src/main/java/com/explordf/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.explordf;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExploRdfApplication.class);
+	}
+
+}
